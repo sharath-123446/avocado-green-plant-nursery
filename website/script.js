e074c6fad39a9041ff7afb0f7b4430b35719b09c
sharath-123446 <sharathbr85@gmail.com>
2025-04-11 20:00:52 +0530

    Delete website directory

--- website/script.js
+++
@@ -1,6 +0,0 @@
-const heroSlider = document.querySelector('#heroSlider');
-const carousel = new bootstrap.Carousel(heroSlider, {
-  interval: 4000,  // 4 seconds
-  ride: 'carousel',
-  pause: false
-});
